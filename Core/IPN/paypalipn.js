46c6990ce5668e95d7188289af07d3df4f653939
aymenbnr <aymenbnr@gmail.com>
2020-05-19 19:42:24 +0200

    updated ipn settings

--- Core/IPN/paypalipn.js
+++ Core/IPN/paypalipn.js
@@ -1,4 +1,4 @@
-var ipn = require('pp-ipn');
+var ipn = require("pp-ipn");
 const {
   CreateLicense,
   DisableLicense,
@@ -6,30 +6,30 @@ const {
 } = require("./LicenseFactory");
 
 const paypalIpn = async (req) => {
-
-    ipn.verify(req.body, function callback(err, msg) {
-        if (err) {
-          console.error(err);
-        } else {
-
-          if (req.body.payment_status == 'Completed') {
-            // Payment has been confirmed as completed
-            const {item_name,first_name,last_name,payer_email,txn_type} = req.body;
-            if((txn_type =="subscr_signup") || (txn_type =="web_accept"))
-            {
-                var license = await CreateLicense({
-                    project: item_name,
-                    fullname: `${first_name} ${last_name}`,
-                    email: payer_email,
-                    source: "paypal",
-                  });
-            }
-           
-              
-          }
+  ipn.verify(req.body, async function callback(err, msg) {
+    if (err) {
+      console.error(err);
+    } else {
+      if (req.body.payment_status == "Completed") {
+        // Payment has been confirmed as completed
+        const {
+          item_name,
+          first_name,
+          last_name,
+          payer_email,
+          txn_type,
+        } = req.body;
+        if (txn_type == "subscr_signup" || txn_type == "web_accept") {
+          var license = await CreateLicense({
+            project: item_name,
+            fullname: `${first_name} ${last_name}`,
+            email: payer_email,
+            source: "paypal",
+          });
         }
-      });
-  
+      }
+    }
+  });
 };
 
 module.exports = { paypalIpn };
--- routes/ipn.js
+++ routes/ipn.js
@@ -1,3 +1,6 @@
+const express = require("express");
+const router = express.Router();
+
 const { ProcessIpnRequest } = require("../Core/ipnProcessor");
 
 router.post("/:endpoint", async (req, res) => {
